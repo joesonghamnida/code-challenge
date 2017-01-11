ab4aa7e61b89d842e80c38e00463318bed2c9a0c
Joe Morris <joe.morris.dev@gmail.com>
2017-01-11 17:29:21 -0600

    all tests pass

--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,20 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(strand_a, strand_b):
+
+    if check_strand_length(strand_a,strand_b) == False:
+        raise ValueError
+
+    difference = 0
+    nucleotide = 0
+    while(nucleotide < len(strand_a)):
+        if strand_a[nucleotide] != strand_b[nucleotide]:
+            difference = difference + 1
+        nucleotide = nucleotide + 1
+
+    return difference
+
+def check_strand_length(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        return False
